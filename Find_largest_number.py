f0c42798cc55472bbdce65393e678f3902f81d81
DataWizVerma <180633049+DataWizVerma@users.noreply.github.com>
2024-11-28 12:37:15 +0530

    num

---
+++ Find_largest_number.py
@@ -0,0 +1,10 @@
+num1=float(input("Enter Your Number 1: "))
+num2=float(input("Enter Your Number 2: "))
+num3=float(input("Enter Your Number 3 : "))
+
+if (num1>num2) and (num1>num3):
+    print("Num1 is greater"),
+elif (num2>num1) and (num2>num3):
+    print("Num2 is greater")
+else:
+    print("Num3 is greater")
