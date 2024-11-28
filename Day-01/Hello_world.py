86607d4d8adc4f6ab547fde97241447053080143
DataWizVerma <180633049+DataWizVerma@users.noreply.github.com>
2024-11-28 11:15:07 +0530

    Hello World

--- Day-01/Hello_world.py
+++ Day-01/Hello_world.py
@@ -1,3 +1,6 @@
+'''solution 01'''
+print("Hello World")
+# -------------------------------------------------------------
 var1 = input("Enter Your Good Name Please: ")
 var2 = "Hello " + var1 + ", Welcome Back to our Website"
 print(var2)
