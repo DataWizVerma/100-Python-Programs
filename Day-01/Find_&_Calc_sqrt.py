4b10ee6da92391f842a9b51596b6b8e8c1085415
DataWizVerma <180633049+DataWizVerma@users.noreply.github.com>
2024-11-28 11:29:18 +0530

    sqrt

---
+++ Day-01/Find_&_Calc_sqrt.py
@@ -0,0 +1,14 @@
+# Solution :01 using exponantion Operator
+
+num=int(input("Enter Your number here: "))
+
+sr=num**(1/2)
+print("The square root of given no is: ",sr)
+
+
+# Solution Using Math Module
+
+import math
+num=int(input("Enter Your Number Here: "))
+sr=math.sqrt(num)
+print("The square root of given no is: ",sr)
