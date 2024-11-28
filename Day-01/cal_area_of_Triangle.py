5242c022f6f695a35c713acaa8ebb53edea0ffba
DataWizVerma <180633049+DataWizVerma@users.noreply.github.com>
2024-11-28 11:40:31 +0530

    area

---
+++ Day-01/cal_area_of_Triangle.py
@@ -0,0 +1,6 @@
+# Formula of area of Triangle is || Area = (1/2) × base × height||
+
+base= float(input("Enter The Base of the Triangle: "))
+height=float(input("Enter The Height of the Triangle: "))
+area=1/2 * base*height
+print(area) 
