de5f7705d180e1abc088e0de064e2dc9d247e5ef
DataWizVerma <180633049+DataWizVerma@users.noreply.github.com>
2024-11-28 12:08:13 +0530

    pn

---
+++ Day-01/Num_is_postive_Negative_or_zero.py
@@ -0,0 +1,10 @@
+# Program to check if a number is positive negative or zero
+
+num=float(input("Enter Your Number here: "))
+
+if num>0:
+    print("It is a Positve Number"),
+elif num==1:
+    print("It is Zero"),
+else:
+    print("It is a negative number")
