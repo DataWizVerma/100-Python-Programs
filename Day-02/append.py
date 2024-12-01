ab400ded3b4b2bcb6a139d699424a6f7d3925922
DataWizVerma <180633049+DataWizVerma@users.noreply.github.com>
2024-12-01 11:35:39 +0530

    append

---
+++ Day-02/append.py
@@ -0,0 +1,12 @@
+'''Question 1:
+Write a Python program to add an element at the end of the list using the append() 
+method.
+
+Example:
+
+Input: [1, 2, 3] and element 4
+Output: [1, 2, 3, 4]'''
+
+num=[3,4,5,2,3,4,5]
+num.append(0)
+print(num)
---
+++ Day-02/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+num=[3,4,5,2,3,4,5]
+num.append(0)
+print(num)
