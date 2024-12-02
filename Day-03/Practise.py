bc51af5082c54f9a28957ae820c1c14228fb41df
DataWizVerma <180633049+DataWizVerma@users.noreply.github.com>
2024-12-02 15:55:38 +0530

    ppp

--- Day-03/Practise.py
+++ Day-03/Practise.py
@@ -18,11 +18,35 @@ print(detail['City'])
 # Question 2:
 # How can you add a new key-value pair to the following dictionary?
 
-# python
-# Copy code
-# student = {
-#     "Name": "Bob",
-#     "Age": 20,
-#     "Grade": "A"
-# }
+student = {
+    "Name": "Bob",
+    "Age": 20,
+    "Grade": "A"
+}
 # Add the key "Subject" with the value "Math" to the dictionary.
+student.update({'Subject':'Maths'})
+print(student)
+
+# Question 3:
+# How would you delete the key-value pair for "Age" from the following dictionary?
+
+student = {
+    "Name": "Alice",
+    "Age": 25,
+    "City": "New York",
+    "Subject": "Math"
+}
+student.remove('Age')
+print("student")
+
+
+# Write a Python program to check if a given key exists in the following dictionary.
+
+student = {
+    "Name": "Alice",
+    "Age": 20,
+    "Grade": "A"
+}
+# Check for the key "Age" and "School"
+print(student.get("Age", "School"))
+
--- Day-03/tempCodeRunnerFile.py
+++ Day-03/tempCodeRunnerFile.py
@@ -1,6 +1,7 @@
-detail={
-    "Name":"Alice",
-    "Age": 25,
-    "City":"New York"
-
+student = {
+    "Name": "Alice",
+    "Age": 20,
+    "Grade": "A"
 }
+# Check for the key "Age" and "School"
+print(student.get("Age" and "School"))
