283d8c09671d5f07cdd980d034d2bda7637927e1
DataWizVerma <180633049+DataWizVerma@users.noreply.github.com>
2024-12-02 16:02:03 +0530

    ok

--- Day-03/Practise.py
+++ Day-03/Practise.py
@@ -50,3 +50,18 @@ student = {
 # Check for the key "Age" and "School"
 print(student.get("Age", "School"))
 
+
+student = {
+    "Name": "John",
+    "Age": 20,
+    "Subjects": ["Math", "English", "History"]
+}
+# Add a new key-value pair: "City" with the value "New York".
+# Change the "Age" to 21.
+# Add "Science" to the "Subjects" list.
+
+student.update({"City": "New York"})
+student.update({"Age": 21})
+student["Subjects"].append("Science")  # Add "Science" to the list
+print(student)
+
--- Day-03/tempCodeRunnerFile.py
+++ Day-03/tempCodeRunnerFile.py
@@ -1,7 +1,13 @@
 student = {
-    "Name": "Alice",
+    "Name": "John",
     "Age": 20,
-    "Grade": "A"
+    "Subjects": ["Math", "English", "History"]
 }
-# Check for the key "Age" and "School"
-print(student.get("Age" and "School"))
+# Add a new key-value pair: "City" with the value "New York".
+# Change the "Age" to 21.
+# Add "Science" to the "Subjects" list.
+
+student.update({"City": "New York"})
+student.update({"Age": 21})
+student["Subjects"].append("Science")  # Add "Science" to the list
+print(student)
