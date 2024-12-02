5914c02f5e4758881391430e4394822a1abf93bb
DataWizVerma <180633049+DataWizVerma@users.noreply.github.com>
2024-12-02 13:28:01 +0530

    dic

---
+++ Day-03/dic.py
@@ -0,0 +1,14 @@
+student={
+    "Name":"Kumar Verma",
+    "marks":{
+        "chemistry": 23,
+        "physics":20,
+        "Maths":33
+    }
+}
+new_dict={"Name":'kumar verma',
+          'age':23,
+          "city":'Lucknow'}
+student.update(new_dict)
+
+print(student)
---
+++ Day-03/tempCodeRunnerFile.py
@@ -0,0 +1,9 @@
+student={
+    "Name":"Kumar Verma",
+    "marks":{
+        "chemistry": 23,
+        "physics":20,
+        "Maths":33
+    }
+}
+print(student.items)
